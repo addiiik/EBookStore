064f4074061f367490c2bdcb3db96707dbac17e5
addiiik <addiii.official@gmail.com>
2026-05-27 11:13:11 +0200

    redirect from vercel url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.17'],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'e-book-store-tau.vercel.app' }],
+        destination: 'https://ebook.adriankukla.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
